c3cf604fb08a116ac94f098edfad66ab8d89d147
Lê Ngọc Trường (Tommy) <wolfcancode@gmail.com>
2022-06-22 03:11:04 +0700

    fix: fixed test file

---
+++ test/login_test.dart
@@ -0,0 +1,25 @@
+// This is a basic Flutter widget test.
+//
+// To perform an interaction with a widget in your test, use the WidgetTester
+// utility in the flutter_test package. For example, you can send tap and scroll
+// gestures. You can also use WidgetTester to find child widgets in the widget
+// tree, read text, and verify that the values of widget properties are correct.
+
+import 'package:eatiplan_mobile/screens/auth/login/login.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:eatiplan_mobile/main.dart';
+
+void main() {
+  Widget createWidgetForTesting({required Widget child}) {
+    return MaterialApp(
+      home: child,
+    );
+  }
+
+  testWidgets('Render main successful', (WidgetTester tester) async {
+    // Build our app and trigger a frame.
+    await tester.pumpWidget(const MyApp());
+  });
+}
