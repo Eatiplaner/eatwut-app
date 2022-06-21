4bf4b5498150cf8bc24044b9f5a08ecbd11caab8
Lê Ngọc Trường (Tommy) <wolfcancode@gmail.com>
2022-06-22 02:17:42 +0700

    feat: finished auth routes

---
+++ lib/screens/home/home.dart
@@ -0,0 +1,48 @@
+import 'package:eatiplan_mobile/screens/auth/login/index.dart';
+import 'package:eatiplan_mobile/screens/auth/register/index.dart';
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: null,
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        child: Align(
+          alignment: Alignment.center,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              ElevatedButton(
+                child: const Text('Login'),
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => const LoginScreen()),
+                  );
+                },
+              ),
+              const SizedBox(height: 10),
+              ElevatedButton(
+                child: const Text('Register'),
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => const RegisterScreen()),
+                  );
+                },
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
