5f9463c9f9a1493b81d38011b167a6a8c584911a
loc.pham@papaya.asia <loc.pham@papaya.asia>
2022-06-26 16:41:43 +0700

    scaffold login markup

--- lib/screens/auth/login/login.dart
+++ lib/screens/auth/login/login.dart
@@ -16,14 +16,83 @@ class LoginScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Login screen'),
       ),
-      body: Center(
-        child: Column(children: [
-          const Text('Register screen'),
-          TextButton(
-            onPressed: doLogin,
-            child: const Text('Test api'),
-          ),
-        ]),
+      body: Container(
+        padding: const EdgeInsets.fromLTRB(40, 100, 40, 40),
+        constraints: const BoxConstraints.expand(),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.fromLTRB(0, 0, 0, 40),
+              child: Container(
+                width: 70,
+                height: 70,
+                padding: const EdgeInsets.all(15),
+                decoration: const BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: Colors.white,
+                ),
+                child: const FlutterLogo(),
+              ),
+            ),
+            const Padding(
+              padding: EdgeInsets.fromLTRB(0, 0, 0, 60),
+              child: Text(
+                "Login Screen",
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black,
+                  fontSize: 30,
+                ),
+              ),
+            ),
+            const Padding(
+              padding: EdgeInsets.fromLTRB(0, 0, 0, 40),
+              child: TextField(
+                style: TextStyle(fontSize: 18, color: Colors.black),
+                decoration: InputDecoration(
+                  labelText: "Username",
+                  labelStyle: TextStyle(
+                    color: Color(0xff888888),
+                    fontSize: 15,
+                  ),
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(0, 0, 0, 40),
+              child: Stack(
+                alignment: AlignmentDirectional.centerEnd,
+                children: const <Widget>[
+                  TextField(
+                    style: TextStyle(fontSize: 18, color: Colors.black),
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      labelText: "Password",
+                      labelStyle: TextStyle(
+                        color: Color(0xff888888),
+                        fontSize: 15,
+                      ),
+                    ),
+                  ),
+                  Text(
+                    "SHOW",
+                    style: TextStyle(
+                      color: Colors.blue,
+                      fontSize: 13,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  )
+                ],
+              ),
+            ),
+            ElevatedButton(
+              onPressed: doLogin,
+              child: const Text("Continue"),
+            ),
+          ],
+        ),
       ),
     );
   }
