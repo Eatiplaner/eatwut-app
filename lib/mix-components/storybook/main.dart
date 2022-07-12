a8d94cfa0932510bd7a7bf0a1f20cb245d8f0981
Louis <huynguyennbk@gmail.com>
2022-07-12 22:51:53 +0700

    fix: lint

--- lib/mix-components/storybook/main.dart
+++ lib/mix-components/storybook/main.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 import 'package:storybook_flutter/storybook_flutter.dart';
 
 class StoryBook extends StatelessWidget {
+  const StoryBook({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) => Storybook(
         stories: [sampleStory],
