cfbbfb4ef8faa12a8b673e8de13e45322d4a1313
Louis <huynguyennbk@gmail.com>
2022-07-14 21:13:13 +0700

    setup: routing management

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
+import 'package:eatiplan_mobile/config/routes.dart';
 import 'package:eatiplan_mobile/config/theme.dart';
-import 'package:eatiplan_mobile/shared/storybook/main.dart';
 import 'package:eatiplan_mobile/provider/theme.dart';
 import 'package:flutter/material.dart';
+import 'package:get/route_manager.dart';
 import 'package:provider/provider.dart';
+import 'package:get/get.dart';
 
 void main() {
   runApp(const MyApp());
@@ -20,29 +22,16 @@ class MyApp extends StatelessWidget {
         ),
       ],
       child: Consumer<ThemeProvider>(
-        builder: (ctx, themeObject, _) => MaterialApp(
+        builder: (ctx, themeObject, _) => GetMaterialApp(
           debugShowCheckedModeBanner: false,
           title: 'Eatiplaner App',
           themeMode: themeObject.mode,
           theme: lightTheme,
           darkTheme: darkTheme,
-          home: const MyHomePage(),
+          initialRoute: '/', // TODO: implement splash screen
+          getPages: appRoutes(),
         ),
       ),
     );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key}) : super(key: key);
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return const Scaffold(body: Center(child: StoryBook()));
   }
 }
---
+++ lib/screens/splash/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        body: Center(
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                MaterialButton(
+                  color: Colors.green,
+                  onPressed: () => Get.toNamed('/storybook'),
+                  child: const Text('View Storybook'),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
