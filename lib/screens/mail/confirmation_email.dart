f2f18e3d93479bb43729915842817eabf7e4f080
Louis <huynguyennbk@gmail.com>
2022-08-27 15:00:52 +0700

    feat: delay clickable on confirmation email screen

--- lib/screens/mail/confirmation_email.dart
+++ lib/screens/mail/confirmation_email.dart
@@ -12,37 +12,46 @@ class ConfirmationEmailScreen extends StatefulWidget {
 
 class _ConfirmationEmailScreen extends State<ConfirmationEmailScreen> {
   String animation = 'success';
+  bool _clickable = false;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       resizeToAvoidBottomInset: false,
-      body: Center(
-          child: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text('mail.confirmation_email.successfully_message'.tr,
-              style: const TextStyle(
-                  fontSize: 15,
-                  color: primaryColor,
-                  fontFamily: fontFamily,
-                  fontWeight: FontWeight.w700)),
-          SizedBox(
-            height: 250,
-            child: FlareActor(
-              'assets/animations/success.flr',
-              animation: animation,
-            ),
-          ),
-          Text('common.tap_anywhere_to_exist'.tr,
-              style: const TextStyle(
-                  fontSize: 14,
-                  color: primaryColor,
-                  fontFamily: fontFamily,
-                  fontWeight: FontWeight.w400))
-        ],
-      )),
+      body: GestureDetector(
+          onTap: () {
+            if (_clickable) {
+              Get.toNamed('/home');
+            }
+          },
+          child: Center(
+              child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text('mail.confirmation_email.successfully_message'.tr,
+                  style: const TextStyle(
+                      fontSize: 15,
+                      color: primaryColor,
+                      fontFamily: fontFamily,
+                      fontWeight: FontWeight.w700)),
+              SizedBox(
+                height: 250,
+                child: FlareActor('assets/animations/success.flr',
+                    animation: animation, callback: (value) {
+                  setState(() {
+                    _clickable = true;
+                  });
+                }),
+              ),
+              Text(_clickable ? 'common.tap_anywhere_to_exist'.tr : '',
+                  style: const TextStyle(
+                      fontSize: 14,
+                      color: primaryColor,
+                      fontFamily: fontFamily,
+                      fontWeight: FontWeight.w400))
+            ],
+          ))),
     );
   }
 }
